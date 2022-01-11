0b1a8fea68935be68d397ac68a468ccf69d2b9ac
Aftabalam37 <itsaftabalam37@gmail.com>
2022-01-11 21:46:05 +0530

    feature added: same email records in tabular form

--- booked.php
+++ booked.php
@@ -7,21 +7,49 @@ $name = $_POST['passenger'];
 $age = $_POST['age'];
 $gender = $_POST['gender'];
 
-$query = "insert into bookings (name, age, gender) values ('$name', $age, '$gender')";
+$email = $_POST['email'];
+
+$query = "insert into bookings (date, name, age, gender, email) values (now(), '$name', $age, '$gender', '$email')";
 
 $return = mysqli_query($connect, $query);
+
+$query1 = "select bookings.* from bookings where bookings.email = '$email'";
+    $result = mysqli_query($connect, $query1);
+    $num = mysqli_num_rows($result);
 
 ?>
 
 <html>
     <head>
         <title>BYT - Book Ticket</title>
+    <style>
+            td {width: 200px;}
+    </style>
     </head>
     <body>
     <?php
     if ($return == 1) {
     ?>
     <h1> Booking Done!</h1>
+    <table>
+        <tr>
+            <td> Date </td>
+            <td> Name </td>
+            <td> Age </td>
+            <td> Gender </td>
+        </tr>
+    <?php
+     for ($i = 1; $i <= $num; $i++){
+         $row = mysqli_fetch_assoc($result);
+         echo "<tr><td> ".$row['date']." </td> <td> ".$row['name']." </td> <td> ".$row['age']." </td> <td> ".$row['gender']. " </td></tr>";
+         
+         
+     }
+   
+    ?>
+
+    </table>
+    <a href="home.php">Add more (+) </a>;
     <?php
      } else{
          echo "<h1> Connection Failed! </h1>";
--- home.php
+++ home.php
@@ -7,6 +7,8 @@ mysqli_select_db($connect, 'bytdb');
 $user = $_POST['user'];
 $email = $_POST['email'];
 
+$_SESSION["email"] = $email;
+
 $query = "insert into logins (user, email) values ('$user','$email')";
 
 $return = mysqli_query($connect, $query);
@@ -38,6 +40,10 @@ $return = mysqli_query($connect, $query);
                 <td><input type="radio" name="gender" value="Male"/>Male
                 <input type="radio" name="gender" value="Female"/>Female 
                 <input type="radio" name="gender" value="Non-Binary"/>Non-Binary</td>
+            </tr>
+            <tr>
+                <td>Email </td>
+                <td ><input type="text" name="email" value="<?php echo $email ?>" readonly></td>
             </tr>
             <tr>
                 <td><input type="submit" value="Book Seat"/></td>
